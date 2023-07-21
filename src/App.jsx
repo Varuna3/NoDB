f12ead261774f33960f2e833a1d1daa4bd01ef00
Payton Kentch <frankrlystan@gmail.com>
2023-07-21 11:24:25 -0600

    Cleaned up syntax A LOT, fixed /plants

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import './index.css'
 import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import NewPlant from './Routes/NewPlant'
-import Plants from './Routes/Plants'
 import axios from 'axios'
 
-const getPlants = async () => {
-  const res = await axios.get('/api/plants')
-  return res
-}
-
-function App() {
-  const [plants, setPlants] = useState([])
-
-  useEffect(() => {
-    axios.get('/api/plants').then(res => {
-      setPlants(res)
-    })
-  }, [])
-
+const App = () => {
   return (
     <BrowserRouter>
       <div id='routes'>
@@ -31,15 +16,75 @@ function App() {
         </button>
       </div>
       <Routes>
-        <Route path='/' element={<h1 />} />
-        <Route path='/plants' element={<Plants Plants={plants} />} />
-        <Route
-          path='/newplant'
-          element={<NewPlant plants={plants} setPlants={setPlants} />}
-        />
+        <Route path='/' element={<h1>Gang Stuff</h1>} />
+        <Route path='/plants' element={<Plants />} />
+        <Route path='/newplant' element={<NewPlant />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
+const Plants = () => {
+  let keys = 0
+  const [plants, setPlants] = useState([])
+  useEffect(() => {
+    axios.get('/api/plants').then(({ data }) => {
+      setPlants([...data])
+    })
+  }, [])
+  const arr = plants.map(e => {
+    keys++
+    return (
+      <div key={keys}>
+        <h1>{e.name}</h1>
+        <img src={e.img}></img>
+        <p>{e.count}</p>
+      </div>
+    )
+  })
+
+  return <>{arr}</>
+}
+
+const NewPlant = () => {
+  const [name, setName] = useState('')
+  const [url, setUrl] = useState('')
+  const [count, setCount] = useState('')
+
+  return (
+    <>
+      <form action='/'>
+        <label htmlFor='name'>Name: </label>
+        <input
+          id='name'
+          type='text'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <label htmlFor='img'>Img URL: </label>
+        <input
+          id='img'
+          type='text'
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+        />
+        <label htmlFor='count'>Count: </label>
+        <input
+          id='count'
+          type='text'
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button
+          onSubmit={() => {
+            // axios.post('/api/newplant')
+          }}
+        />
+      </form>
+    </>
+  )
+}
+
+export { Plants, NewPlant }
 
 export default App
